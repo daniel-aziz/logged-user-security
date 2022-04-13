bf7acbab553b4ba78c55a604e9728c7e2a670a6a
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-04-13 22:38:01 +0300

    added testing

--- src/main/java/com/jb/loggedusersecurity/LoggedUserSecurityApplication.java
+++ src/main/java/com/jb/loggedusersecurity/LoggedUserSecurityApplication.java
@@ -1,13 +1,45 @@
 package com.jb.loggedusersecurity;
 
+import com.jb.loggedusersecurity.domain.Role;
+import com.jb.loggedusersecurity.domain.User;
+import com.jb.loggedusersecurity.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class LoggedUserSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoggedUserSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoggedUserSecurityApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+
+            userService.saveUser(new User(null, "A","a","1", new ArrayList<>()));
+            userService.saveUser(new User(null, "B","b","2", new ArrayList<>()));
+            userService.saveUser(new User(null, "C","c","3", new ArrayList<>()));
+            userService.saveUser(new User(null, "D","d","4", new ArrayList<>()));
+
+            userService.addRoleToUser("a", "ROLE_USER");
+            userService.addRoleToUser("a", "ROLE_MANAGER");
+            userService.addRoleToUser("b", "ROLE_ADMIN");
+            userService.addRoleToUser("c", "ROLE_ADMIN");
+            userService.addRoleToUser("c", "ROLE_SUPER_ADMIN");
+            userService.addRoleToUser("d", "ROLE_USER");
+
+
+
+        };
+    }
 }
---
+++ src/main/java/com/jb/loggedusersecurity/domain/RoleToUserForm.java
@@ -0,0 +1,13 @@
+package com.jb.loggedusersecurity.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleToUserForm {
+    private String username;
+    private String roleName;
+}
