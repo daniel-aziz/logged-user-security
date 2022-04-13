2976d12a2f0729371d0ec5fc051104b4ee919592
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-04-13 20:39:10 +0300

    created userResource

--- src/main/java/com/jb/loggedusersecurity/api/UserResource.java
+++ src/main/java/com/jb/loggedusersecurity/api/UserResource.java
@@ -1,4 +1,24 @@
 package com.jb.loggedusersecurity.api;
 
+import com.jb.loggedusersecurity.domain.User;
+import com.jb.loggedusersecurity.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
 }
