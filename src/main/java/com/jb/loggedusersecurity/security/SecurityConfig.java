648efc7000d355aa9a631bd25c8390f3f5a5d25a
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-04-14 20:06:52 +0300

    created securityConfig

---
+++ src/main/java/com/jb/loggedusersecurity/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.jb.loggedusersecurity.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfiguration {
+
+}
